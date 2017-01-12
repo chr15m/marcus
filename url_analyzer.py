c57a2904ca93630392a3d0a0011472c1c6f30966
Chris McCormick <chris@mccormick.cx>
2017-01-12 15:46:30 +0800

    Better URL tokenizing.

--- url_analyzer.py
+++ url_analyzer.py
@@ -1,58 +1,53 @@
-import re
 import urlparse
 
-from whoosh.analysis import LowercaseFilter, StopFilter, Token, Tokenizer
-
-query_re = re.compile('[ +&=]')
+from whoosh.analysis import LowercaseFilter, StopFilter, Token, Tokenizer, StandardAnalyzer
 
 class UrlTokenizer(Tokenizer):
-	def __call__(self, value, start_pos=0, start_char=0, positions=False, **kwargs):
-		parts = urlparse.urlparse(value)
+    def __call__(self, value, start_pos=0, start_char=0, positions=False, **kwargs):
+        ana = StandardAnalyzer()
+        parts = urlparse.urlparse(value)
 
-		#t = Token(text=parts.scheme, boost=0.5)
-		#if positions:
-		#	t.pos = start_pos + 1
-		#yield t
+        #t = Token(text=parts.scheme, boost=0.5)
+        #if positions:
+        #    t.pos = start_pos + 1
+        #yield t
 
-		hostport = parts.netloc.split(':')
-		hostparts = hostport.pop(0).split(".");
+        hostport = parts.netloc.split(':')
+        hostparts = hostport.pop(0).split(".");
 
-		#print hostparts
-		for p in hostparts:
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
+        #print hostparts
+        for p in hostparts:
+            t = Token(text=p, boost=1.0)
+            if positions:
+                t.pos = start_pos + 1
+            yield t
 
-		for x in range(len(hostparts) - 1):
-                        p = ".".join(hostparts[x:])
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
+        for x in range(len(hostparts) - 1):
+            p = ".".join(hostparts[x:])
+            t = Token(text=p, boost=1.0)
+            if positions:
+                t.pos = start_pos + 1
+            yield t
          
-		for p in [x for x in parts.path.split('/') if x]:
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
+        if parts.path:
+            for p in [x.text for x in ana(parts.path) if x.text]:
+                t = Token(text=p, boost=1.0)
+                if positions:
+                    t.pos = start_pos + 1
+                yield t
 
-		for p in [x for x in parts.fragment.split(' ') if x]:
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
+        if parts.fragment:
+            for p in [x.text for x in ana(parts.fragment) if x.text]:
+                t = Token(text=p, boost=1.0)
+                if positions:
+                    t.pos = start_pos + 1
+                yield t
 
-		for p in [x for x in query_re.split(parts.query) if x]:
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
-
-		for p in [x for x in parts.fragment.split(' ') if x]:
-			t = Token(text=p, boost=1.0)
-			if positions:
-				t.pos = start_pos + 1
-			yield t
+        if parts.query:
+            for p in [x.text for x in ana(parts.query) if x.text]:
+                t = Token(text=p, boost=1.0)
+                if positions:
+                    t.pos = start_pos + 1
+                yield t
 
 url_analyzer = UrlTokenizer() | LowercaseFilter() | StopFilter()
